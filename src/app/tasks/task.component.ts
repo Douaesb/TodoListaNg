4b49e9ed21215cc516e3a38f0be6f24c45e018ef
Douae sb <douae.sb411@gmail.com>
2024-12-27 19:18:16 +0100

    TOD-11-compoent-task

--- src/app/tasks/task.component.ts
+++ src/app/tasks/task.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TasksModel} from "../models/task.model";
+import {TaskService} from "../services/task.service";
+import {TaskListComponent} from "./task-list/task-list.component";
+import {TaskFormComponent} from "./task-form/task-form.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [
+    TaskListComponent,
+    TaskFormComponent,
+    NgIf
+  ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
-export class TaskComponent {
+export class TaskComponent  {
+  tasks: TasksModel[] = [];
+  showFormPopup = false;
+  taskToEdit: TasksModel | null = null;
+
+  constructor(private readonly taskService: TaskService) {
+    this.fetchTasks()
+  }
+
+
+  fetchTasks() {
+    this.tasks = this.taskService.getTasks()
+  }
+
+
+  addTask() {
+    this.showFormPopup = true;
+    this.taskToEdit = null;
+  }
+
+
+  editTask(task: TasksModel) {
+    this.showFormPopup = true;
+    this.taskToEdit = task;
+  }
+
+  deleteTask(task: TasksModel) {
+    if (confirm(`Are you sure you want to delete the category "${task.title}"?`)) {
+      this.taskService.deleteTask(task)
+      this.fetchTasks();
+    }
+  }
+
+  handleFormSubmitted() {
+    this.showFormPopup = false;
+    this.fetchTasks();
+  }
 
 }
